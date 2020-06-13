cc911fbfb9333a88c629a85551f1a411a223b1b6
podkolzinmir <podkolzinmiriam@gmail.com>
2020-06-12 23:07:09 -0400

    google visualization chart

--- d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
+++ d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
@@ -17,29 +17,25 @@ $('#switch-hitters').sheetrock({
 
 
 // making a tree
-      google.charts.load('current', {packages:["orgchart"]});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {packages:["orgchart"]});
+google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Name');
-        data.addColumn('string', 'Manager');
-        data.addColumn('string', 'ToolTip');
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Name');
+  data.addColumn('string', 'Big');
 
-        // For each orgchart box, provide the name, manager, and tooltip to show.
-        data.addRows([
-          [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'},
-           '', 'The President'],
-          [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'},
-           'Mike', 'VP'],
-          ['Alice', 'Mike', ''],
-          ['Bob', 'Jim', 'Bob Sponge'],
-          ['Carol', 'Bob', '']
-        ]);
+  // For each orgchart box, provide the name, manager, and tooltip to show.
+  data.addRows([
+    ['Mike',''],
+    ['Jim','Mike'],
+    ['Alice', 'Mike'],
+    ['Bob', 'Jim']
+    ['Carol', 'Bob']
+  ]);
 
-        // Create the chart.
-        var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
-        // Draw the chart, setting the allowHtml option to true for the tooltips.
-        chart.draw(data, {'allowHtml':true});
-      }
- 
+  // Create the chart.
+  var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+  // Draw the chart, setting the allowHtml option to true for the tooltips.
+  chart.draw(data, {'allowHtml':true});
+}
