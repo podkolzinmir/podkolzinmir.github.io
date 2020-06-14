03563314df9adb9effa93dc5351e34a9efafc5e6
podkolzinmir <podkolzinmiriam@gmail.com>
2020-06-13 23:27:34 -0400

    jesus

--- d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
+++ d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
@@ -6,17 +6,27 @@
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Name');
         data.addColumn('string', 'Manager');
-        data.addColumn('string', 'ToolTip');
+
+        var j = "Jess Giangrande";
+        var julia = "Julia Earl";
+        var katie = "Katie Tirabassi";
+        var briana = "Briana Maetta";
+        var miriam = "Miriam Podkolzin";
+        var d = "Dani Ragan";
+
+    
 
         // For each orgchart box, provide the name, manager, and tooltip to show.
-        data.addRows([
-          [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'},
-           '', 'The President'],
-          [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'},
-           'Mike', 'VP'],
-          ['Alice', 'Mike', ''],
-          ['Bob', 'Jim', 'Bob Sponge'],
-          ['Carol', 'Bob', '']
+       data.addRows([
+         [j, "Caitlyn Labonte"],
+         [julia, j],
+         [katie, j],
+         [briana, katie],
+         [miriam, julia],
+         [d, miriam],
+         ["Caitlyn Labonte", "Michelle Plotzker"],
+         ["Michelle Plotzker", "Sarah Byrne"],
+         ["Sarah Byrne, Shannon McGrath"]
         ]);
 
         // Create the chart.
