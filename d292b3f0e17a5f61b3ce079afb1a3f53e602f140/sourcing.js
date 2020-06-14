567ccd530ec4bbc884aa22a5a8efdc3f9aabf79f
podkolzinmir <podkolzinmiriam@gmail.com>
2020-06-13 23:19:15 -0400

    back to og

--- d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
+++ d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
@@ -1,53 +1,26 @@
-// Sheetrock.js 1.0 Example 2
-// https://chriszarate.github.io/sheetrock/
 
-// Let’s look at switch hitters and rank them by batting average. 
-// We’ll only grab the columns we care about and fetch just the 
-// top ten to help focus the reader’s attention.
-
-// Define spreadsheet URL.
-var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1qT1LyvoAcb0HTsi2rHBltBVpUBumAUzT__rhMvrz5Rk/edit#gid=0';
-
-
-
-
-// making a tree
-google.charts.load('current', {packages:["orgchart"]});
-google.charts.setOnLoadCallback(drawChart);
+      google.charts.load('current', {packages:["orgchart"]});
+      google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Name');
         data.addColumn('string', 'Manager');
-
-        var j = "Jess Giangrande";
-        var julia = "Julia Earl";
-        var katie = "Katie Tirabassi";
-        var briana = "Briana Maetta";
-        var miriam = "Miriam Podkolzin";
-        var d = "Dani Ragan";
-
-    
+        data.addColumn('string', 'ToolTip');
 
         // For each orgchart box, provide the name, manager, and tooltip to show.
-       data.addRows([
-         [j, "Caitlyn Labonte"],
-         [julia, j],
-         [katie, j],
-         [briana, katie],
-         [miriam, julia],
-         [d, miriam],
-         ["Caitlyn Labonte", "Michelle Plotzker"],
-         ["Michelle Plotzker", "Sarah Byrne"],
-         ["Sarah Byrne, Shannon McGrath"]
-        
+        data.addRows([
+          [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'},
+           '', 'The President'],
+          [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'},
+           'Mike', 'VP'],
+          ['Alice', 'Mike', ''],
+          ['Bob', 'Jim', 'Bob Sponge'],
+          ['Carol', 'Bob', '']
         ]);
-  
 
-
-
-  // Create the chart.
-  var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
-  // Draw the chart, setting the allowHtml option to true for the tooltips.
-  chart.draw(data, {'allowHtml':true});
-}
+        // Create the chart.
+        var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+        // Draw the chart, setting the allowHtml option to true for the tooltips.
+        chart.draw(data, {'allowHtml':true});
+      }
