051291301d10fd0dac14924eb6b22027db211177
Miriam Podkolzin <49803313+podkolzinmir@users.noreply.github.com>
2020-06-15 17:02:37 -0400

    Update sourcing.js

--- d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
+++ d292b3f0e17a5f61b3ce079afb1a3f53e602f140/sourcing.js
@@ -1,32 +1,22 @@
 
-      google.charts.load('current', {packages:["orgchart"]});
+       google.charts.load('current', {packages:["orgchart"]});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Name');
         data.addColumn('string', 'Manager');
-
-        var j = "Jess Giangrande";
-        var julia = "Julia Earl";
-        var katie = "Katie Tirabassi";
-        var briana = "Briana Maetta";
-        var miriam = "Miriam Podkolzin";
-        var d = "Dani Ragan";
-
-    
+        data.addColumn('string', 'ToolTip');
 
         // For each orgchart box, provide the name, manager, and tooltip to show.
-       data.addRows([
-         [j, "Caitlyn Labonte"],
-         [julia, j],
-         [katie, j],
-         [briana, katie],
-         [miriam, julia],
-         [d, miriam],
-         ["Caitlyn Labonte", "Michelle Plotzker"],
-         ["Michelle Plotzker", "Sarah Byrne"],
-         ["Sarah Byrne, Shannon McGrath"]
+        data.addRows([
+          [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'},
+           '', 'The President'],
+          [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'},
+           'Mike', 'VP'],
+          ['Alice', 'Mike', ''],
+          ['Bob', 'Jim', 'Bob Sponge'],
+          ['Carol', 'Bob', '']
         ]);
 
         // Create the chart.
@@ -34,3 +24,4 @@
         // Draw the chart, setting the allowHtml option to true for the tooltips.
         chart.draw(data, {'allowHtml':true});
       }
+ 
