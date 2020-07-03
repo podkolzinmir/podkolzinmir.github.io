01ad54985e035d6d9cb5e578aec3ea442f7b84d6
podkolzinmir <podkolzinmiriam@gmail.com>
2020-07-02 22:39:10 -0400

    adding page

---
+++ b1432db0088f669789237117d35b0b8e14768d10/sourcing.js
@@ -0,0 +1,10 @@
+
+// JVD button
+ document.getElementById("jvdButton").onclick = function () {
+  location.href = "https://podkolzinmir.github.io/JVD/index.html";
+};
+
+// JM button
+document.getElementById("jmButton").onclick = function () {
+  location.href = "https://podkolzinmir.github.io/JM/index.html";
+};
